659a0564077184725b6c975db14d93b889e5d85b
Adam <nohbody5@gmail.com>
2022-06-21 10:27:25 -0600

    Rearrange Prime Folder, Add notes to Prime

--- PrimeGenerator/prime.py
+++ PrimeGenerator/prime.py
@@ -1,9 +1,36 @@
 #!/usr/bin/env python3
+
+import logging
+
+pr = True   # Set to True to have results printed to screen in real time (Slow)
+lg = False  # Set to True to have results logged to file (Faster)
+
+if lg: logging.basicConfig(filename='prime.log',
+                            level='DEBUG',
+                            format='%(message)s',
+                            filemode='w')
+
+
 '''
-This Program will calculate wether a number is prime and print only prime numbers.
-The program will then attempt to find the next prime number counting upwards from 1.
-This will show that calculating very large primes as used in cryptography would take a very
-long time.
+This Program will calculate wether a number is prime and print only prime
+numbers. The program will then attempt to find the next prime number counting
+upwards from 1. This will show that calculating very large primes as used in
+cryptography would take a very long time.
+
+In reality we dont fully factor every prime we use, instead using an algorithm
+to predict probable primes.
+
+Also Sieve algorithm would likely be much faster than the brute force we are
+doing here, but even with Sieve it would still take a very long time to
+test all possible factors of a 1024 bit RSA prime.
+
+Python printing and logging can be very resource intensive as well. By
+eliminating one or both of these functions we can dramatically improve our
+script's performance, but we then lose the ability to see what the program is
+doing!
+
+Sieve of Eratosthenes:
+https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 '''
 
 # Start and end point if limited run is needed
@@ -20,11 +47,13 @@ while True: # Endless
     root_i = i ** 0.5 # optimized 2
 
     if i == 2:
-        print(f'Prime Found!: {i}')
+        if lg: logging.debug(f'Prime Found!: {i}')
+        if pr: print(f'Prime Found!: {i}')
         continue
         
     if i % 2 == 0:
-        # print(f"Not a prime: {i}")
+        # logging.debug(f'Not a Prime: {i}) # Un-comment to log non-primes
+        # print(f'Not a prime: {i}')        # Un-comment to print non-primes
         continue
 
     elif root_i == int(root_i):
@@ -33,10 +62,12 @@ while True: # Endless
     while n < i:
         
         if i % n == 0 and n != 1:
-            # print(f"Not a prime: {i}")
+
+            # print(f"Not a prime: {i}") # Un-comment to print non-primes
             break
-        elif n >= root_i: # Optimized 2
-            print(f'Prime Found!: {i}')
-            break         # Optimized 2
+        elif n >= root_i:
+            if lg: logging.debug(f'Prime Found!: {i}')
+            if pr: print(f'Prime Found!: {i}')
+            break
         
         n += 1 
